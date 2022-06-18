5119e818bc7fb8ee82db149d96590d27219312ea
RomanovVS <105357348+RomanovVS@users.noreply.github.com>
2022-06-18 10:33:31 +0300

    Add files via upload

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,26 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.DAO.CarDAO;
+
+@Controller
+@RequestMapping("/cars")
+public class CarController {
+    private final CarDAO carDAO;
+
+    @Autowired
+    public CarController(CarDAO carDAO) {
+        this.carDAO = carDAO;
+    }
+
+    @GetMapping()
+    public String showCars(@RequestParam(value = "count", required = false, defaultValue = "0") Long count, Model model) {
+        model.addAttribute("car", carDAO.showCars(count));
+        return "cars";
+    }
+}
